1b8267df7334156f8b0b2280583ea93ca41ec09f
Hacker3112 <35932311+pextonjack@users.noreply.github.com>
2023-10-11 16:35:02 +0100

    Added Ultrasonic capabilities
    Added sensor data reading code from https://maxbotix.com/blogs/basic-use/maxbotix-ultrasonic-sensor-for-2024-first-robotics-teams

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,10 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.AnalogInput;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -23,6 +27,32 @@ public class Robot extends TimedRobot {
   private final XboxController m_controller = new XboxController(0);
   private final Timer m_timer = new Timer();
 
+  public DigitalOutput ultrasonicTriggerPinOne = new DigitalOutput(0);
+  public DigitalOutput ultrasonicTriggerPinTwo = new DigitalOutput(1);
+ 
+  public AnalogInput ultrasonicSensorOne = new AnalogInput(0);
+  public AnalogInput ultrasonicSensorTwo = new AnalogInput(1);
+ 
+  public double ultrasonicSensorOneRange = 0;
+  public double ultrasonicSensorTwoRange = 0;
+ 
+  public double voltageScaleFactor = 1;
+ 
+  public void turnOnSensorOne() {
+    ultrasonicTriggerPinOne.set(true);
+    ultrasonicTriggerPinTwo.set(false);
+  }
+ 
+  public void turnOnSensorTwo() {
+    ultrasonicTriggerPinOne.set(false);
+    ultrasonicTriggerPinTwo.set(true);
+  }
+ 
+  public void turnOffBothSensors() {
+    ultrasonicTriggerPinOne.set(false);
+    ultrasonicTriggerPinTwo.set(false);
+  }
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -33,17 +63,35 @@ public class Robot extends TimedRobot {
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
     m_rightDrive.setInverted(true);
+
+    //Initialize range readings on SmartDashboard as max distance in Centimeters.
+    SmartDashboard.putNumber("Sensor 1 Range", 500);
+    SmartDashboard.putNumber("Sensor 2 Range", 500);
+  }
+
+  @Override
+  public void robotPeriodic() {
+    //Publish range readings to SmartDashboard
+    SmartDashboard.putNumber("Sensor 1 Range", ultrasonicSensorOneRange);
+    SmartDashboard.putNumber("Sensor 2 Range", ultrasonicSensorTwoRange);
+ 
+    voltageScaleFactor = 5/RobotController.getVoltage5V(); //Calculate what percentage of 5 Volts we are actually at
   }
 
   /** This function is run once each time the robot enters autonomous mode. */
   @Override
   public void autonomousInit() {
     m_timer.restart();
+    //If we are in Autonomous mode, turn on the first sensor (and turn off the second sensor)
+    turnOnSensorOne();
   }
 
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
+    //Get a reading from the first sensor, scale it by the voltageScaleFactor, and then scale to Centimeters
+    ultrasonicSensorOneRange = ultrasonicSensorOne.getValue()*voltageScaleFactor*0.125;
+    
     // Drive for 2 seconds
     if (m_timer.get() < 2.0) {
       // Drive forwards half speed, make sure to turn input squaring off
@@ -55,12 +103,24 @@ public class Robot extends TimedRobot {
 
   /** This function is called once each time the robot enters teleoperated mode. */
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+    //If we are in Teleoperated mode, turn on the second sensor (and turn off the first sensor)
+    turnOnSensorTwo();
+  }
 
   /** This function is called periodically during teleoperated mode. */
   @Override
   public void teleopPeriodic() {
     m_robotDrive.arcadeDrive(-m_controller.getLeftY(), -m_controller.getRightX());
+    
+    //Get a reading from the second sensor, scale it by the voltageScaleFactor, and then scale to Centimeters
+    ultrasonicSensorTwoRange = ultrasonicSensorTwo.getValue()*voltageScaleFactor*0.125;
+  }
+
+  @Override
+  public void disabledInit() {
+    //If the robot is disabled, turn off both sensors
+    turnOffBothSensors();
   }
 
   /** This function is called once each time the robot enters test mode. */
